d391c585268a380fca60e9e6fc43c073d614f643
multimediachavez-cloud <multimediachavez@gmail.com>
2026-04-15 00:38:55 -0500

    Update run.py

--- run.py
+++ run.py
@@ -5,4 +5,5 @@ app = create_app()
 
 if __name__ == "__main__":
     debug = os.getenv("FLASK_DEBUG", "0") == "1"
-    app.run(debug=debug)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=debug)
